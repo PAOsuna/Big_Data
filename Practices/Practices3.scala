f066621eb915352734a9fd5cf1b85c9e96d78c0a
Pablo Osuna <pablo.osuna16@tectijuana.edu.mx>
2020-10-16 18:29:37 -0700

    Se creo la practica 3 y se hizo el algoritmo 1 de fibonacci

---
+++ Practices/Practices3.scala
@@ -0,0 +1,24 @@
+//Fibonacci 1
+def fib1(n:Int):Int = if(n<2) 
+    1
+    else fib(n-1)+fib(n-2)
+
+(fib1(5))
+
+//fib in which int returns an int is equal if n is less than 2, returns 1, else n minus 1, plus, the fib of n minus 2
+
+//Fibonacci 2
+import scala.math.sqrt
+def sqrt(25)
+pow(5.0,2.0)
+pow 5
+
+def fib2(n:Int):Int = if (n<2) 1 else 
+def p: Double = ((1 + sqrt(5))/2) 
+def j: Int = (p(pow n) - ((1 - p)pow n) / (sqrt (5))) 3
+
+(fib2(5))
+
+sqrt (5)
+
+math.pow(5)
