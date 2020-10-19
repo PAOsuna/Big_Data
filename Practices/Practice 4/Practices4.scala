d7cab937342be6261549d740e83991953f1f1a16
daniel521221 <leonardo.sanudo17@tectijuana.edu.mx>
2020-10-18 18:25:44 -0700

    add fibonacci algoritm 3 iterative version

--- Practices/Practice 4/Practices4.scala
+++ Practices/Practice 4/Practices4.scala
@@ -22,3 +22,23 @@ def j: Int = (p(pow n) - ((1 - p)pow n) / (sqrt (5))) 3
 sqrt (5)
 
 math.pow(5)
+
+//Algorithm 3 Iterative version
+
+def fib3(n:Int):Int={
+var n : Int = 7
+var a = 0
+var b = 1
+var c = 0
+var k = 0 
+
+
+    for(k <- 1 to n) {
+        
+        c = b + a
+        a = b
+        b = c 
+    }
+     return a
+}
+println(fib3(n))
