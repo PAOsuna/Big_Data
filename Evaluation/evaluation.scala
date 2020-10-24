640e5e28867ec1c4a1c219d28b214b5a3006c3c0
Pablo Osuna <pablo.osuna16@tectijuana.edu.mx>
2020-10-24 14:26:57 -0700

    se hizo la parte restante del examen

--- Evaluation/evaluation.scala
+++ Evaluation/evaluation.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
  
 val spark = SparkSession.builder().getOrCreate() // Pregunta 1 Comienze una simple sesion de spark
 
-val df = spark.read.option("header", "true").option("inferSchema","true")csv("/home/leonardo/DatosMasivos/Big_Data/Evaluation") //inferimos el esquema
+val df = spark.read.option("header", "true").option("inferSchema","true")csv("/home/alejandro/Escritorio/ExamenDatosMasivos/Netflix.csv") //inferimos el esquema
 
 df.columns // Pregunta 3 mostramos las columnas
 
@@ -14,3 +14,45 @@ df.describe().show()// Pregunta 6 funcion describe
 
 val df2 = df.withColumn("HV Ratio", df("High")+df("Volume")) //Pregunta 7 creacion de nuevo dataframe 
 df2.show()
+
+// 8.- ¿Qué día tuvo el pico mas alto en la columna “Close”?
+df.orderBy($"Close".desc).show(1)
+
+// 9.- Escribe con tus propias palabras en un comentario de tu codigo. ¿Cuál es el
+// significado de la columna Cerrar “Close”?
+
+// 10.- ¿Cuál es el máximo y mínimo de la columna “Volume”?
+df.select(max("Volume")).show()
+df.orderBy(desc("Volume")).show(1)
+
+df.select(min("Volume")).show()
+df.orderBy(asc("Volume")).show(1)
+
+// 11.- Con Sintaxis Scala/Spark $ conteste los siguiente:
+
+    //Hint: Basicamente muy parecido a la session de dates, tendran que crear otro
+    //dataframe para contestar algunos de los incisos.
+
+    // a. ¿Cuántos días fue la columna “Close” inferior a $ 600?
+    df.filter($"close"<600).count()
+
+    // b. ¿Qué porcentaje del tiempo fue la columna “High” mayor que $ 500?
+    
+    val porcentaje1 = df.filter($"High">500).count()
+    val porcentaje2 = df.filter($"High">0).count()
+    val porcentaje3:Double = porcentaje1 * 100
+    porcentaje3 / porcentaje2
+
+    // c. ¿Cuál es la correlación de Pearson entre columna “High” y la columna “Volumen”?
+    df.select(corr("High", "Volume")).show
+
+    // d. ¿Cuál es el máximo de la columna “High” por año?
+    import spark.implicits._
+    val year1 = df.withColumn("Year", year(df("Date")))
+    val year2 = year1.select($"Year", $"High").groupBy("Year").max()
+    year2.select($"Year", $"max(High)").orderBy(asc("Year")).show()
+
+    // e. ¿Cuál es el promedio de columna “Close” para cada mes del calendario?
+    val mes1 = df.withColumn("Mes", month(df("Date")))
+    val mes2 = mes1.select($"Mes", $"Close").groupBy("Mes").avg()
+    mes2.select($"Mes", $"avg(Close)").orderBy(asc("Mes")).show()
